5e9db165bf2902769e5e173c11a0f68d5b5b26a6
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-15 03:03:07 +0900

    Inject fontawesome code

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faVolumeMute, faVolumeUp, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faVolumeMute, faVolumeUp, faCodeBranch);
+
+Vue.component('fa-icon', FontAwesomeIcon);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+	el: '#app',
+	render: h => h(App)
+});
