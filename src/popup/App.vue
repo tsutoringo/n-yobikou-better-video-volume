de49d5d1b0b903f790ba570934273fd163d5f4a2
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-15 03:12:12 +0900

    Completed for the time being

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,20 +1,48 @@
 <template>
-	<div>
-		volume: <input type="range" max="1" step="0.000000001" v-model="volume"><br>
-		muted: <input type="checkbox" v-model="muted">
+	<div id="app">
+		<main>
+		</main>
+		<div class="controller">
+			<div class="volume-range">
+				<input type="range" max="1" step="0.000000001" v-model="volume">
+			</div>
+			<div class="detail">
+				<span class="vol">
+					音量設定 <input class="vol-num" type="number" max="100" min="0" v-model="volNum">%
+				</span>
+				<span class="mute">
+					<label>
+						<input type="checkbox" v-model="muted">
+						<fa-icon v-if="muted" icon="volume-mute" />
+						<fa-icon v-else icon="volume-up" />
+					</label>
+				</span>
+			</div>
+		</div>
+		<v-footer />
 	</div>
 </template>
 
 <script>
-
+import VFooter from './Footer';
 export default {
 	name: 'App',
-	components: { },
+	components: { VFooter },
 	data: () => ({
 		volume: 0,
 		muted: false,
 		port: null
 	}),
+	computed: {
+		volNum: {
+			set (val) {
+				this.volume = Math.min(val / 100, 1);
+			},
+			get () {
+				return this.volume * 100;
+			}
+		}
+	},
 	watch: {
 		muted () {
 			this.setState();
@@ -48,10 +76,84 @@ export default {
 	}
 }
 </script>
-
 <style>
 html {
-	width: 400px;
-	height: 400px;
+	width: 300px;
+}
+
+body {
+	margin: 0;
+}
+
+#app {
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	background-color: #607D8B;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+
+main {
+	height: auto;
+	flex: 1;
+}
+
+.controller .detail {
+	padding: 4px;
+	color: #ececec;
+	display: flex;
+}
+
+.controller .detail .vol {
+	margin-right: auto;
+}
+
+.controller .detail .mute {
+	transform: scale(1.4);
+}
+
+.controller .detail .mute input {
+	display: none;
+}
+
+.vol-num {
+	min-width: 100px;
+}
+</style>
+<style>
+
+a {
+	color: #81C784;
+	text-decoration: none;
+}
+
+input[type="text"],input[type="number"] {
+	color: #ececec;
+	outline: none;
+	border: none;
+	background-color: transparent;
+}
+
+input[type="range"] {
+	box-sizing: border-box;
+	-webkit-appearance: none;
+	appearance: none;
+	cursor: pointer;
+	outline: none; 
+	height: 3px; 
+	width: 100%; 
+	background: #ffffff;
+	margin: 5px 0;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+	box-sizing: border-box;
+	-webkit-appearance: none;
+	background: #ffffff;
+	width: 11px; 
+	height: 11px;
+	border-radius: 50%;
 }
 </style>
---
+++ src/popup/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+	<footer>
+		<fa-icon icon="code-branch" class="icon"/>
+		<span class="repository">
+			<a href="https://github.com/tsutoringo/n-yobikou-better-video-volume" target="_blank">n-yobikou-better-video-volume</a>
+		</span>
+		@
+		<span class="commit-hash"><a :href="`https://github.com/tsutoringo/n-yobikou-better-video-volume/commit/${commitHash}`" target="_blank">{{commitHash}}</a></span>
+	</footer>
+</template>
+<script>
+export default {
+	name: 'v-footer',
+	computed: {
+		commitHash () {
+			return process.env.VUE_APP_GIT_HASH
+		}
+	}
+}
+</script>
+<style scoped>
+footer {
+	color: white;
+	padding: 5px;
+	background-color: #37474F;
+}
+footer .icon {
+	margin-right: 5px;
+	color: #81C784;
+}
+</style>
